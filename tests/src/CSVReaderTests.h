51bc0b8797c3a92d49b4a115d9883a727daa960d
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-10 20:30:19 +0100

    Fix test code after changes to dependencies

--- tests/src/CSVReaderTests.h
+++ tests/src/CSVReaderTests.h
@@ -7,7 +7,7 @@
 #ifndef _ISHIKO_CPP_CSV_TESTS_CSVREADERTESTS_H_
 #define _ISHIKO_CPP_CSV_TESTS_CSVREADERTESTS_H_
 
-#include <Ishiko/Tests/Core.hpp>
+#include <Ishiko/TestFramework/Core.hpp>
 
 class CSVReaderTests : public Ishiko::TestSequence
 {
