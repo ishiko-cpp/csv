778b86a9b438a4c80f72d791ff1b663eb464f6d3
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2023-01-03 20:58:19 +0000

    Updates

--- include/Ishiko/CSV.hpp
+++ include/Ishiko/CSV.hpp
@@ -4,8 +4,8 @@
     See https://github.com/ishiko-cpp/csv/blob/main/LICENSE.txt
 */
 
-#ifndef _GUARD_ISHIKO_CPP_CSV_HPP
-#define _GUARD_ISHIKO_CPP_CSV_HPP
+#ifndef GUARD_ISHIKO_CPP_CSV_HPP
+#define GUARD_ISHIKO_CPP_CSV_HPP
 
 #include "CSV/CSVReader.hpp"
 #include "CSV/linkoptions.hpp"
